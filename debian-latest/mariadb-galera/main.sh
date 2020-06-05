9a9f6ea4cf93beed81e0c98f342a8a0e8e34635a
The_Moose <paa93@users.noreply.github.com>
2020-06-05 13:11:02 +0200

    Create main.sh

---
+++ debian-latest/mariadb-galera/main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Update and install mariadb latest version
+apt-get update && apt-get upgrade --yes
+apt-get install --yes mariadb-server mariadb-client rsync nload htop
+
+# Change bind adress to 0.0.0.0 for global access
+sed -i 's/bind-address/#bind-address/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+# Setup a 2GB swap space
+dd if=/dev/zero of=/root/swap.img bs=1024 count=2097152
+chmod 600 /root/swap.img
+mkswap /root/swap.img
+swapon /root/swap.img
+
+# Download and install galera.cnf
