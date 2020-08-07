4accb9a2f25db83008bed119bd407b0bb9579cfd
The_Moose <paa93@users.noreply.github.com>
2020-08-07 20:28:32 +0200

    Update mariadb.bash

--- debian-latest/mariadb-galera/mariadb.bash
+++ debian-latest/mariadb-galera/mariadb.bash
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Add repository for mariadb 10.5
-sudo apt-get install software-properties-common dirmngr -y
-sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-sudo add-apt-repository 'deb [arch=amd64] https://mirrors.nxthost.com/mariadb/repo/10.5/debian buster main'
+# Using Debian 10 default repo 10.3
+#sudo apt-get install software-properties-common dirmngr -y
+#sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+#sudo add-apt-repository 'deb [arch=amd64] https://mirrors.nxthost.com/mariadb/repo/10.5/debian buster main'
 
 # Install mariadb server plus galera
 sudo apt-get update && sudo apt-get install mariadb-server -y
@@ -17,3 +17,4 @@ sudo rm 60-galera.cnf
 sudo wget https://raw.githubusercontent.com/99stack/installer/master/debian-latest/mariadb-galera/60-galera.cnf
 
 # Finishing up, to start a new cluster, run:    sudo galera_new_cluster
+# Then restart the server and let other nodes connect.
