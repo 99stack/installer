1511a09826cce5867bc3139ea63ff70f23e46faa
The_Moose <paa93@users.noreply.github.com>
2020-08-08 22:49:34 +0200

    Update syncthing.bash

--- debian-latest/syncthing/syncthing.bash
+++ debian-latest/syncthing/syncthing.bash
@@ -2,7 +2,7 @@
 
 # Add key and repo for latest version of Syncthing
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+echo "deb https://apt.syncthing.net/ syncthing release" | sudo tee /etc/apt/sources.list.d/syncthing.list
 
 # Update system and packages
 sudo apt-get update && sudo apt-get upgrade -y
