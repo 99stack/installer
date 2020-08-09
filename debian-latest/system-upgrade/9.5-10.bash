0754c9cec1de6fd9ac5f97416a671843cd707ffb
The_Moose <paa93@users.noreply.github.com>
2020-08-09 15:52:54 +0200

    Update 9.5-10.bash

--- debian-latest/system-upgrade/9.5-10.bash
+++ debian-latest/system-upgrade/9.5-10.bash
@@ -12,6 +12,9 @@ sudo apt-get update && sudo apt-get upgrade -y
 # Run dist-upgrade to resolve remaining dependencies
 sudo apt-get dist-upgrade
 
+# Install packages for monitoring, maintenance and clock sync
+sudo apt-get -y install curl rsync htop ntp nmap nload zip unzip git apt-transport-https gnupg2 ca-certificates lsb-release
+
 # Clean up unused packages 
 sudo apt-get autoremove -y
 
